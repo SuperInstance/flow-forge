a852861fbb1c71156c140e9620cbd74a747ddba0
Lucineer <magnus.digennaro@gmail.com>
2026-04-07 13:36:42 -0800

    security: add Content-Security-Policy with frame-ancestors
    Superinstance & Lucineer (DiGennaro et al.)

--- src/worker.ts
+++ src/worker.ts
@@ -221,6 +221,6 @@ export default {
       return json({ saved: true, name: flow.name });
     }
 
-    return new Response(getLanding(), { headers: { 'Content-Type': 'text/html;charset=UTF-8', ...CSP } });
+    return new Response(getLanding(), { headers: { 'Content-Type': 'text/html;charset=UTF-8', 'Content-Security-Policy': 'default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https:; connect-src 'self' https://*; frame-ancestors 'none';', ...CSP } });
   }
 };
